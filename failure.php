15d359f7883a6b7a714c9c26660c233f0684ceca
matiasbravo1 <50522569+matiasbravo1@users.noreply.github.com>
2020-06-11 00:02:16 -0300

    Update failure.php

--- failure.php
+++ failure.php
@@ -1,3 +1,4 @@
-<?php
-echo "Fallida";
-?>
+<center><h3 style="margin-top:30px">Transacción Fallida</h3>
+<p style="margin-top:20px">Payment Type: <?php echo $_GET["payment_type"];?></p>
+<p>External Reference: <?php echo $_GET["external_reference"];?></p>
+<p>Collection Id: <?php echo $_GET["collection_id"];?></p></center>
