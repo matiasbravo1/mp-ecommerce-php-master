5711f0dcaadc2742a75175bfd0784fce091d3553
matiasbravo1 <50522569+matiasbravo1@users.noreply.github.com>
2020-06-11 00:51:11 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -36,12 +36,18 @@ $payer->phone = array(
     "area_code" => '11',    
     "number" => '22223333'
 );
-$payer->address = (object) array(
-    "street_name" => 'False',
-    "street_number" => 123,
-    "zip_code" => '1111'
-);
 
+// $payer->address = (object) array(
+//     "street_name" => 'False',
+//     "street_number" => 123,
+//     "zip_code" => '1111'
+// );
+
+$address = new stdClass();
+$address->zip_code = "1111";
+$address->street_name = "False";
+$address->street_number = 123;
+$payer->address = $address;
 
 $preference = new MercadoPago\Preference();
 
