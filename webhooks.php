007d1b97fc1962cd07c6901864578ef37106c2e6
matiasbravo1 <50522569+matiasbravo1@users.noreply.github.com>
2020-06-11 00:21:07 -0300

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,4 +1,5 @@
 <?php
+//Este archivo es una copia del que está en emercari.net/MercadoPago, ya que en heroku no pude registrar los resultados.
 
 http_response_code(200);
 
@@ -7,7 +8,19 @@ foreach ($_GET as $key => $value) {
 }
 
 $myfile = fopen("webhooks_output.txt", "a");
-fwrite($myfile, date('m/d/Y h:i:s a', time()) . $response.'\n');
+fwrite($myfile, date('m/d/Y h:i:s a', time()) . " " . $response);
 fclose($myfile);
 
+
+if ($_GET["topic"] == 'payment'){
+
+	$curl = "curl -X GET 'https://api.mercadopago.com/v1/payments/".$_GET["id"]."?access_token=APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398'";
+  
+	$output = shell_exec($curl); 
+
+	$myfile = fopen("webhooks_output2.txt", "w");
+	fwrite($myfile, $output);
+	fclose($myfile);
+
+}
 ?>
